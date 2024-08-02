6b23b0f92134e94efff4ffa4064e73f98ab4270e
Omar Ehab <113807991+OmarEhab262@users.noreply.github.com>
2024-08-02 14:11:07 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Nav from "./components/Nav";
 import About from "./pages/About";
-import Cursor from "./pages/Cursor";
 import FavoriteFruits from "./pages/FavoriteFruits";
 import FruitShop from "./pages/FruitShop";
 import Home from "./pages/Home";
@@ -15,7 +14,6 @@ function App() {
       <FruitShop />
       <About />
       <Footer />
-      <Cursor />
     </>
   );
 }
